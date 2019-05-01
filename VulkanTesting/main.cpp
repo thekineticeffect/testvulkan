d5f1f8d84486479719a1c1c8cedd1b24f4b55fd2
Avinash Vakil <m@avinashvakil.com>
2019-04-30 19:53:11 -0700

    Initial commit. Builds and runs.

--- VulkanTesting/main.cpp
+++ VulkanTesting/main.cpp
@@ -1,15 +1,35 @@
-//
-//  main.cpp
-//  VulkanTesting
-//
-//  Created by Avinash Vakil on 4/30/19.
-//  Copyright © 2019 Avinash Vakil. All rights reserved.
-//
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    glfwInit();
+    
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+    
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+    
+    std::cout << extensionCount << " extensions supported" << std::endl;
+    
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+    
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+    
+    glfwDestroyWindow(window);
+    
+    glfwTerminate();
+    
     return 0;
 }
